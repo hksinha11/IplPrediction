62ca765891489b6c5d1cd742a6d8eb563800b731
hksinha11 <hksinha11@users.noreply.github.com>
2017-05-12 16:13:58 +0530

    Update computeCost.py

--- computeCost.py
+++ computeCost.py
@@ -11,7 +11,7 @@ def computeCost(mytheta,X,y): #Cost function
     #note to self: *.shape is (rows, columns)
     return float((1./(2*m)) * np.dot((h(mytheta,X)-y).T,(h(mytheta,X)-y)))
 
-#Test that running computeCost with 0's as theta returns 32.07:
+
 
 initial_theta = np.zeros((X.shape[1],1)) #(theta is a vector with n rows and 1 columns (if X has n features) )
 print computeCost(initial_theta,X,y)
