f88bb17dc7cd22f9612f4d342d9a2064189bf637
hksinha11 <hksinha11@users.noreply.github.com>
2017-05-12 16:17:21 +0530

    Update cricPredict.py

--- cricPredict.py
+++ cricPredict.py
@@ -4,6 +4,6 @@ plt.figure(figsize=(10,6))
 plt.plot(X[:,1],y[:,0],'rx',markersize=10,label='Training Data')
 plt.plot(X[:,1],myfit(X[:,1]),'b-',label = 'Hypothesis: h(x) = %0.2f + %0.2fx'%(theta[0],theta[1]))
 plt.grid(True) #Always plot.grid true!
-plt.ylabel('Profit in $10,000s')
-plt.xlabel('Population of City in 10,000s')
+plt.ylabel('Score after 20 overs for teams!')
+plt.xlabel('Score in 1 to 6 overs!')
 plt.legend()
